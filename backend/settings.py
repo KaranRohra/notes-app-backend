bdfeb66b53afc76d14bdc1dbdcb0dcef59e0c8bd
KaranRohra <rohrakaran38@gmail.com>
2021-07-15 20:11:56 +0530

    Udpated allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["notes-store-backend-api.herokuapp.com", "localhost"]
 
 
 # Application definition
